9909ef03350e56373364b858cd8d79c653df1db1
CHOSSEANG <jng00153@gmail.com>
2025-08-08 08:08:42 +0900

    stopwatch 프로젝트 > 초기 세팅

---
+++ stopwatch/script.js
@@ -0,0 +1,9 @@
+const appendSeconds = document.getElementById('seconds');
+const appendTens = document.getElementById('tens');
+const buttonStart = document.getElementById('button-start');
+const buttonStop = document.getElementById('button-stop');
+const buttonReset = document.getElementById('button-reset');
+
+let seconds = 0;
+let tens = 0;
+let interval;
