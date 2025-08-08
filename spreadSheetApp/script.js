42e9ea801951a7a6e391f4e11f18abaa0e789e75
CHOSSEANG <jng00153@gmail.com>
2025-08-09 01:58:13 +0900

    spreadsheet 앱 프로젝트 > spreadsheet 초기 배열 생성

---
+++ spreadSheetApp/script.js
@@ -0,0 +1,18 @@
+const spreadSheetContainer = document.getElementById('spreadsheet-container');
+const ROWS = 10;
+const COLS = 10;
+
+const spreadsheet= [];
+
+function initSpreadSheet(){
+    for(let i =0; i < ROWS; i++){
+        let spreadsheetRow = [];
+        for(let j = 0; j < COLS; j++){
+            spreadsheetRow.push(i + "-" + j);
+        }
+        spreadsheet.push(spreadsheetRow);
+    }
+    console.log(spreadsheet);
+}
+
+initSpreadSheet();
