0d9aceb59d0eefb6181e6cd60d0495d5c179f7b7
CHOSSEANG <jng00153@gmail.com>
2025-08-14 00:37:37 +0900

    개인미션 > 배열 및 출력 - 이름, 나이 입력 받는 함수 추가

--- expmission/arrayApplied/script.js
+++ expmission/arrayApplied/script.js
@@ -4,6 +4,13 @@ userArray.push({name:"John",age:10});
 userArray.push({name:"Thomas",age:30});
 userArray.push({name:"Neo",age:20});
 
+function inputNameAge(){
+    const name = prompt('이름을 입력하세요');
+    const age = prompt('나이를 입력하세요');
+
+    userArray.push({name:name,age:Number(age)});
+}
+
 function findAllArray(){
     userArray.forEach((user)=>{
         console.log(user);
@@ -19,6 +26,7 @@ function ageAverage(){
     return value / userArray.length;
 }
 
+inputNameAge();
 console.log('findAllArray');
 findAllArray();
 console.log('age Average')
